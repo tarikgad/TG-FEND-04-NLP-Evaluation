7f82ae0cbc95411f0b76f1ec49c6c6b98b1b607f
Tarik Gad <36502125+tarikgad@users.noreply.github.com>
2022-02-11 13:32:23 +0200

    Update index.js

--- src/client/index.js
+++ src/client/index.js
@@ -1,11 +1,11 @@
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
 
-import './styles/base.scss'
-import './styles/footer.scss'
-import './styles/form.scss'
-import './styles/header.scss'
-import './styles/resets.scss'
+import './styles/base.css'
+import './styles/footer.css'
+import './styles/form.css'
+import './styles/header.css'
+import './styles/resets.css'
 
 export{
     checkForName,
